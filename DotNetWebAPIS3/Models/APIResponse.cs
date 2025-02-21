7111767c6d19e2d1275cede984a3438d21b3179d
Denzel Awuah <denzelbawuah@gmail.com>
2025-02-21 11:28:42 -0500

    Model entity updates

--- DotNetWebAPIS3/Models/APIResponse.cs
+++ DotNetWebAPIS3/Models/APIResponse.cs
@@ -1,9 +1,18 @@
 ﻿namespace DotNetWebAPIS3.Models
 {
+    /// <summary>
+    /// Custome API Response to return to clients of the API
+    /// </summary>
     public class APIResponse
     {
+        /// <summary>
+        /// The name of the file that was created or modified
+        /// </summary>
         public string FileName { get; set; }
 
+        /// <summary>
+        /// The string message to return in the response
+        /// </summary>
         public string Message { get; set; }
 
         public APIResponse(string filename, string message)
